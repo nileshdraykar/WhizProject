3b99873c4853ac0fa6d1ca91b9528061bfbcf1c6
Nilesh Raykar <nileshdraykar@gmail.com>
2020-09-22 19:09:02 +0530

    committing all final changes

--- WhizProject/Target/Pub-Megaconf_builder.sh
+++ WhizProject/Target/Pub-Megaconf_builder.sh
@@ -18,11 +18,8 @@ AppStore_IOS_EXPORT_OPTIONS_PLIST="Pub-Megaconf_exportOption.plist"
 Firebase_IOS_EXPORT_OPTIONS_PLIST="Pub-Megaconf_exportOption_Ad_Hoc.plist"
 FIRBASE_APP_ID="1:704600838549:ios:863f8d46e37a2d35"
 cd $cur_dir/../
-#echo "Setting version to 1.0, 1"
-#xcrun agvtool new-version -all 1.0
-#xcrun agvtool new-marketing-version 1.0
-pwd
-#git pull master
+
+pwd  # used for print current directory
 git status
 git remote update
 UPSTREAM=${1:-'@{u}'}
@@ -31,10 +28,31 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+    echo "Up-to-date No changes found on Remote"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
     git pull
+    xcrun agvtool next-version -all
+    git commit -m "update build version"
+    git push
+    echo "Archiving the project"
+    xcodebuild archive -project $cur_dir/../../${PROJECT_NAME}.xcodeproj -scheme $IOS_SCHEME -configuration $IOS_CONFIGURATION -derivedDataPath $cur_dir/../../../build -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive
+    unset GEM_HOME
+    unset GEM_PATH
+
+    #echo "Export archive to create IPA file using $Firebase_IOS_EXPORT_OPTIONS_PLIST"
+    #xcodebuild -exportArchive -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive -exportOptionsPlist $cur_dir/$Firebase_IOS_EXPORT_OPTIONS_PLIST -exportPath $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/Firebase
+    #
+    #firebase appdistribution:distribute $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/Firebase/$IOS_SCHEME.ipa  \
+    #--app $FIRBASE_APP_ID  \
+    #--release-notes-file $cur_dir/../Release-Notes.txt  --groups-file $cur_dir/../Testers-Group.txt
+
+
+    echo "Export archive to create IPA file using $AppStore_IOS_EXPORT_OPTIONS_PLIST"
+    xcodebuild -exportArchive -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive -exportOptionsPlist $cur_dir/$AppStore_IOS_EXPORT_OPTIONS_PLIST -exportPath $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/App_Store
+
+    #xcrun altool --upload-app -f $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/App_Store/$IOS_SCHEME.ipa -u $apple_id -p $password —output-format xml
+
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
     git commit -m "update build version"
@@ -42,20 +60,3 @@ elif [ $REMOTE = $BASE ]; then
 else
     echo "Diverged"
 fi
-echo "Archiving the project"
-xcodebuild archive -project $cur_dir/../../${PROJECT_NAME}.xcodeproj -scheme $IOS_SCHEME -configuration $IOS_CONFIGURATION -derivedDataPath $cur_dir/../../../build -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive
-unset GEM_HOME
-unset GEM_PATH
-
-#echo "Export archive to create IPA file using $Firebase_IOS_EXPORT_OPTIONS_PLIST"
-#xcodebuild -exportArchive -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive -exportOptionsPlist $cur_dir/$Firebase_IOS_EXPORT_OPTIONS_PLIST -exportPath $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/Firebase
-#
-#firebase appdistribution:distribute $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/Firebase/$IOS_SCHEME.ipa  \
-#--app $FIRBASE_APP_ID  \
-#--release-notes-file $cur_dir/../Release-Notes.txt  --groups-file $cur_dir/../Testers-Group.txt
-
-
-echo "Export archive to create IPA file using $AppStore_IOS_EXPORT_OPTIONS_PLIST"
-xcodebuild -exportArchive -archivePath $cur_dir/../../../build/Products/${PROJECT_NAME}.xcarchive -exportOptionsPlist $cur_dir/$AppStore_IOS_EXPORT_OPTIONS_PLIST -exportPath $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/App_Store
-
-#xcrun altool --upload-app -f $cur_dir/../../../build/Products/IPA/$IOS_SCHEME/App_Store/$IOS_SCHEME.ipa -u $apple_id -p $password —output-format xml
