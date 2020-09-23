aa9043768ea48207aa978003b5df03d46ba77fe7
Nilesh Raykar <nileshdraykar@gmail.com>
2020-09-23 09:32:18 +0530

    commited changes from master branch 2

--- WhizProject/ViewController.m
+++ WhizProject/ViewController.m
@@ -20,7 +20,7 @@
     NSLog(@"Added new changes in this file");
     
     NSLog(@"final changess sdf");
-    NSLog(@"commited changes from master branch");
+    NSLog(@"commited changes from master branch 2");
 }
 
 
